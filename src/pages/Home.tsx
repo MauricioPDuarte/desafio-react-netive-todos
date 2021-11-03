c56612c7db6db691f01f4b09d3451c27d03a6d67
Mauricio Pruss Duarte <desenvolvimento8@rzsistemas.com.br>
2021-11-03 19:32:08 -0300

    Subindo alterações

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks(tasks => [...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(tasks => tasks.map((task) => {
+      if (task.id == id) {
+        task.done = !task.done;
+      }
+
+      return task;
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks => tasks.filter(task => task.id != id));
   }
 
   return (
@@ -26,10 +38,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
